8f1d5988c7f06cb97ccaa48c28876e8aa66ac464
Ram2896 <ramachari2896@gmail.com>
2020-11-24 18:51:48 +0530

    Animation changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 // import * as AOS from 'aos';
 import { DeviceDetectorSer } from 'src/app/service/device-detector.service';
 declare var $: any;
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,9 +14,15 @@ export class AppComponent {
   constructor(public _deviceDetectorService :DeviceDetectorSer ) {
   }
   ngOnInit(){
+    AOS.init({
+      offset: 200,
+      duration: 100,
+      easing: "ease-in-sine",
+      delay: 100
+    });
     // AOS.init();
     setTimeout(() => {
       this.showLoader = false;
-    }, 3000);
+    }, 4000);
   }
 }
